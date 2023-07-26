2d8fcece2664e7e4ad1356d901c3e8d092e91212
12Nikita09 <loucle@mail.ru>
2023-07-26 15:59:28 +0300

    add block

---
+++ index.js
@@ -0,0 +1,6 @@
+// let buttonEdit = document.querySelector('.profile__btn-edit');
+// let buttonSave = document.querySelector('.entrance__save');
+// let blockEntrance = document.querySelector('.entrance');
+// buttonEdit.addEventListener("click", function (e) {
+//     blockEntrance.classList.toggle('visible')
+// });
