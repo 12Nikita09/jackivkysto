be999ea550ce95bb2b5aa7c69093108313c078c7
12Nikita09 <loucle@mail.ru>
2023-08-20 17:26:26 +0300

    change entrance

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 // let buttonSave = document.querySelector('.entrance__save');
 // let blockEntrance = document.querySelector('.entrance');
 // buttonEdit.addEventListener("click", function (e) {
-//     blockEntrance.classList.toggle('visible')
+//     blockEntrance.classList.toggle('visible');
+    
 // });
+// buttonSave.addEventListener("click", function (e) {
+//     blockEntrance.classList.toggle('visible');
+    
+// });
+
+
